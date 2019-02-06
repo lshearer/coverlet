741e5e7357ca4cac93c06f83fc315754737b99bf
Lyle Shearer <lyle.shearer@gmail.com>
2019-02-06 15:35:11 -0700

    Push coverage results for comparison

--- coverage-viewer-issue-repro.sh
+++ coverage-viewer-issue-repro.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $cwd
 
+# Build project according to CONTRIBUTING.md
 dotnet msbuild build.proj
+# Attempt to generate coverage report
 npx coverage-viewer test/coverlet.core.tests/coverage.json -s . -o ./coverage 
